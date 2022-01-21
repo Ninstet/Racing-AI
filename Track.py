a903f7faee6e19b79399070b6ee25569833d3ecb
Ninstet <jxw1025@student.bham.ac.uk>
2022-01-21 20:08:47 +0000

    Improvements to the Track class and added .gitignore

--- Track.py
+++ Track.py
@@ -2,40 +2,73 @@ import pyglet
 import numpy as np
 
 class Track:
-    def __init__(self, vertices = []):
+    def __init__(self):
         self.track_vertices = []
-        self.lines = []
-        self.track = []
+        self.line_shapes = []
+        self.track_shapes = []
 
-        for vertex in vertices:
-            self.create_track(vertex[0], vertex[1])
+    def __str__(self):
+        return str(self.track_vertices)
+
+    def __repr__(self):
+        return str(self.track_vertices)
 
     def create_track(self, x, y):
-        self.track_vertices.append((x, y))
+        '''
+        Adds a vertex to the track.
+        '''
 
-        if len(self.track_vertices) % 2 == 0 and len(self.track_vertices) > 0:
-            line = pyglet.shapes.Line(self.track_vertices[-1][0], self.track_vertices[-1][1], self.track_vertices[-2][0], self.track_vertices[-2][1], 3, color=(255, 0, 0))
+        # If the vertex being placed is the first vertex
+        if len(self.track_vertices) == 0:
+            self.track_vertices.append([(x, y)])
+
+        # If the first vertex in the pair has already been placed
+        elif len(self.track_vertices[-1]) == 1:
+            self.track_vertices[-1].append((x, y))
+            self.update_shapes()
+
+        # If the pair of vertices is complete and a new vertex needs to be created
+        elif len(self.track_vertices[-1]) == 2:
+            self.track_vertices.append([(x, y)])
+
+    def update_shapes(self):
+        '''
+        Updates the shapes based on the most recent vertex.
+        '''
+
+        # If the pair of vertices is complete
+        if len(self.track_vertices[-1]) == 2:
+
+            # Draw reward line between two vertices
+            new_pair = self.track_vertices[-1]
+
+            line = pyglet.shapes.Line(new_pair[0][0], new_pair[0][1], new_pair[1][0], new_pair[1][1], 3, color=(255, 0, 0))
             line.opacity = 100
-            self.lines.append(line)
+            self.line_shapes.append(line)
 
+            # If this is not the first reward line
             if len(self.lines) > 1:
-                shape_1 = pyglet.shapes.Triangle(self.track_vertices[-1][0], self.track_vertices[-1][1], self.track_vertices[-2][0], self.track_vertices[-2][1], self.track_vertices[-3][0], self.track_vertices[-3][1], color=(145, 145, 145))
-                shape_2 = pyglet.shapes.Triangle(self.track_vertices[-2][0], self.track_vertices[-2][1], self.track_vertices[-3][0], self.track_vertices[-3][1], self.track_vertices[-4][0], self.track_vertices[-4][1], color=(145, 145, 145))
+
+                # Draw joining track triangles
+                old_pair = self.track_vertices[-2]
+
+                shape_1 = pyglet.shapes.Triangle(new_pair[0][0], new_pair[0][1], new_pair[1][0], new_pair[1][1], old_pair[0][0], old_pair[0][1], color=(145, 145, 145))
+                shape_2 = pyglet.shapes.Triangle(new_pair[1][0], new_pair[1][1], old_pair[0][0], old_pair[0][1], old_pair[1][0], old_pair[1][1], color=(145, 145, 145))
                 shape_1.opacity = 100
                 shape_2.opacity = 120
 
-                self.track.append(shape_1)
-                self.track.append(shape_2)
+                self.track_shapes.append(shape_1)
+                self.track_shapes.append(shape_2)
 
-                self.lines.append(pyglet.shapes.Line(self.track_vertices[-1][0], self.track_vertices[-1][1], self.track_vertices[-3][0], self.track_vertices[-3][1], 3, color=(0, 0, 0)))
-                self.lines.append(pyglet.shapes.Line(self.track_vertices[-2][0], self.track_vertices[-2][1], self.track_vertices[-4][0], self.track_vertices[-4][1], 3, color=(0, 0, 0)))
+                # Draw track edges
+                self.line_shapes.append(pyglet.shapes.Line(new_pair[0][0], new_pair[0][1], old_pair[0][0], old_pair[0][1], 3, color=(0, 0, 0)))
+                self.line_shapes.append(pyglet.shapes.Line(new_pair[1][0], new_pair[1][1], old_pair[1][0], old_pair[1][1], 3, color=(0, 0, 0)))
 
-        print(self.track_vertices)
-    
-    def save(self, filename):
-        np.savetxt(filename + ".txt", np.array(self.track_vertices), fmt='%s', delimiter=', ')
-    
     def clear(self):
+        '''
+        Clear / reset the track.
+        '''
+
         for line in self.lines:
             line.delete()
         for shape in self.track:
@@ -44,11 +77,37 @@ class Track:
         self.track_vertices = []
         self.lines = []
         self.track = []
+
+    def save(self, filename):
+        '''
+        Save the track to a text file.
+        '''
+
+        if len(self.track_vertices[-1]) == 2:
+
+            # Convert 3D array into 2D array
+            track_vertices = np.array(self.track_vertices)
+            track_vertices_raw = track_vertices.reshape(track_vertices.shape[0], -1)
+
+            # Save file
+            np.savetxt(filename + ".txt", track_vertices_raw, delimiter=', ', fmt='%s')
+
+        else:
+            print("Track incomplete!")
     
     def load(self, filename):
+        '''
+        Load the track from a text file.
+        '''
+
         self.clear()
 
-        vertices = np.loadtxt(filename + ".txt", delimiter=', ')
+        # Load file
+        track_vertices_raw = np.loadtxt(filename + ".txt", delimiter=', ', dtype=float).astype(int)
 
-        for vertex in vertices:
-            self.create_track(vertex[0], vertex[1])
+        # Convert 2D array into 3D array
+        track_vertices = track_vertices_raw.reshape(track_vertices_raw.shape[0], 2, 2)
+
+        for pair in track_vertices:
+            self.create_track(pair[0][0], pair[0][1])
+            self.create_track(pair[1][0], pair[1][1])
